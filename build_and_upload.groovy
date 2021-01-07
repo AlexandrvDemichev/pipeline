80fd2cedb3aaf08c5ca3a2799f406e7b169ba0c5
Alexandr <snekx1@mail.com>
2021-01-07 12:29:46 +0300

    cc

--- build_and_upload.groovy
+++ build_and_upload.groovy
@@ -52,7 +52,7 @@ pipeline {
                                       usernameVariable: 'username')]) {
       
     
-                            def resp = sh(script: 'curl -X POST "http://localhost:8081/service/rest/v1/components?repository=Nexus_PROD" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "maven2.groupId=Nexus_PROD" -F "maven2.artifactId='+{ID_system}+'" -F "maven2.version='+{version}+'" -F "maven2.generate-pom=true" -F "maven2.packaging=zip" -F "maven2.asset1=@file.zip;type=application/zip" -F "maven2.asset1.classifier=distrib" -F "maven2.asset1.extension=zip" -u ${username}:${password}', returnStdout: true)        
+                            def resp = sh(script: 'curl -v -X POST "http://localhost:8081/service/rest/v1/components?repository=Nexus_PROD" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "maven2.groupId=Nexus_PROD" -F "maven2.artifactId='+{ID_system}+'" -F "maven2.version='+{version}+'" -F "maven2.generate-pom=true" -F "maven2.packaging=zip" -F "maven2.asset1=@file.zip;type=application/zip" -F "maven2.asset1.classifier=distrib" -F "maven2.asset1.extension=zip" -u ${username}:${password}', returnStdout: true)        
                                 echo "Response: "+resp.toString()
                             if(!(resp =~ '204' || resp =~ '200')){
 
