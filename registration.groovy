3f7a4c43146122fb702083b7a0f561100aef85f6
Alexandr <snekx1@mail.com>
2021-01-07 13:35:39 +0300

    ff

--- registration.groovy
+++ registration.groovy
@@ -15,9 +15,19 @@ pipeline {
             steps{
                 script{
                     echo 'Search distrib'
-                    def resp = sh(script: 'PGPASSWORD=postgres psql -h 127.0.0.1 -p 5432 -U postgres -d nexus_distrib_monitoring -c "select * from distrib_list where id_system = \''+id_system+'\'"', returnStdout: true)
-                    
-                    echo resp
+                    try{
+                        def resp = sh(script: 'PGPASSWORD=postgres psql -h 127.0.0.1 -p 5432 -U postgres -d nexus_distrib_monitoring -c "select * from distrib_list where id_system = \''+id_system+'\'"', returnStdout: true)
+                        
+                        echo resp
+                        if(resp =~ 'row 0'){
+                            currentBuild.result='ABORTED'
+                            return
+                        }
+                    }catch(Exception ee){
+                            echo 'EXCEPTION: ' + ee.getMessage()
+                            currentBuild.result='ABORTED'
+                            return
+                    }
                         
                 }
             }
